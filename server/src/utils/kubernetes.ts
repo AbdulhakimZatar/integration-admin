a15202bc655ce3c7b7605f16672b4feb2a7be8a2
Abdulhakim Zatar <abdulhakim.zatar@pwc.com>
2022-12-12 13:45:43 +0300

    Server - initial setup

--- server/src/utils/kubernetes.ts
+++ server/src/utils/kubernetes.ts
@@ -1,7 +1,11 @@
-import k8s from "@kubernetes/client-node";
+import k8s from '@kubernetes/client-node';
 
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
-kc.loadFromFile("../../.kube/config");
+kc.loadFromFile('../../.kube/config');
 
 export default kc;
+
+function test() {
+console.log('test');
+}
